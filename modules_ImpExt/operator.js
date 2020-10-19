736fb35aa7022ccdaadadfc128a928354f889de1
Sai Aditya <dsaiaditya@gmail.com>
2020-10-19 19:13:04 +0530

    added os, modules, path, npm, modulewrapper

---
+++ modules_ImpExt/operator.js
@@ -0,0 +1,18 @@
+const add = (a,b) => {
+    return a+b;
+};
+
+const sub = (a,b) => {
+    return a-b;
+}
+
+const mul = (a,b) => {
+    return a*b;
+};
+
+const divi = (a,b) => {
+    return a/b;
+};
+
+module.exports = {add, sub, mul, divi};
+
